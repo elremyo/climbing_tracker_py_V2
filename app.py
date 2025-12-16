79c51a206a1e425202f65cd76a45310fde0cd54b
Remy <remy.currau@gmail.com>
2025-12-16 21:51:09 +0100

    Use icons instead of emojis in top menu buttons

--- app.py
+++ app.py
@@ -4,26 +4,26 @@ import streamlit as st
 st.set_page_config(
     page_title="Climbing Tracker",
     page_icon="🧗",
-    layout="wide"
+    layout="centered"
 )
 
 st.header("⛰️ Climbing tracker",anchor=False, divider="orange", text_alignment="center",width="content")
 
 # définition des pages
 pages = [
-    st.Page("pages/dashboard_page.py", title="Tableau de bord", icon="📊"),
-    st.Page("pages/routes_page.py", title="Voies", icon="🧗"),
-    st.Page("pages/attempts_page.py", title="Tentatives", icon="🎯"),
-    st.Page("pages/route_detail_page.py", title="Détail voie", icon="🔍")
+    st.Page("pages/dashboard_page.py", title="Tableau de bord", icon=":material/analytics:"),
+    st.Page("pages/routes_page.py", title="Voies", icon=":material/tools_ladder:"),
+    st.Page("pages/attempts_page.py", title="Tentatives", icon=":material/target:"),
+    st.Page("pages/route_detail_page.py", title="Détail voie", icon=":material/search:")
 
 ]
 
 current = st.navigation(pages,position="hidden")
 
 with st.container(horizontal=True,gap="small", vertical_alignment="center"):
-    st.page_link("pages/dashboard_page.py", label="Dashboard", icon="📊")
-    st.page_link("pages/routes_page.py", label="Voies", icon="🧗")
-    st.page_link("pages/attempts_page.py", label="Tentatives", icon="🎯")
+    st.page_link("pages/dashboard_page.py", label="Dashboard", icon=":material/analytics:")
+    st.page_link("pages/routes_page.py", label="Voies", icon=":material/tools_ladder:")
+    st.page_link("pages/attempts_page.py", label="Tentatives", icon=":material/target:")
 
 
 current.run()
